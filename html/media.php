3ed851e26b2292270727442defc205ed63270761
Robert J Saulnier <robert.j.saulnier@gmail.com>
2017-01-24 22:25:31 -0400

    Changed media links to multi-lines to make it look better

--- html/media.php
+++ html/media.php
@@ -1,32 +1,50 @@
 <?php
 $body_en = <<<MEDIA_EN
-<ul>
-    <li>
+<article>
+    <h1>
         <a href="http://ici.radio-canada.ca/widgets/mediaconsole/medianet/7118804">
-            Téléjournal Acadie, 15 juillet 2014, Une famille victime d'intimidation au NB (à 50 secondes dans le reportage)
+            Une famille victime d'intimidation au NB<br/>(à 50 secondes dans le reportage)
         </a>
-    </li>
-    <li>
+    </h1>
+    <cite>Téléjournal Acadie</cite>
+    <time>15 juillet 2014</time>
+</article>
+<article>
+    <h1>
         <a href="http://www.radio-canada.ca/widgets/mediaconsole/medianet/7118827">
-            Téléjournal Acadie, 21 juillet 2014, La famille Clavet de retour en cours (à 4 minutes 20 secondes dans le reportage)
+            La famille Clavet de retour en cours<br/>(à 4m20s dans le reportage)
         </a>
-    </li>
-    <li>
+    </h1>
+    <cite>Téléjournal Acadie</cite>
+    <time>21 juillet 2014</time>
+</article>
+<article>
+    <h1>
         <a href="http://ici.radio-canada.ca/widgets/mediaconsole/medianet/7162102">
-            Téléjournal Acadie, 8 octobre 2014, Surveillance communautaire à Cocagne (à 3m23s dans le reportage)
+            Surveillance communautaire à Cocagne<br/>(à 3m23s dans le reportage)
         </a>
-    </li>
-    <li>
+    </h1>
+    <cite>Téléjournal Acadie</cite>
+    <time>8 octobre 2014</time>
+</article>
+<article>
+    <h1>
         <a href="http://ici.radio-canada.ca/widgets/mediaconsole/medianet/7293057">
-            Téléjournal Acadie, 29 juin 2015, Victimes d'intimidation à Cocagne (à 1m40s dans le reportage)
+            Victimes d'intimidation à Cocagne<br/>(à 1m40s dans le reportage)
         </a>
-    </li>
-    <li>
+    </h1>
+    <cite>Téléjournal Acadie</cite>
+    <time>29 juin 2015</time>
+</article>
+<article>
+    <h1>
         <a href="http://www.acadienouvelle.com/mon-opinion/2014/11/17/soutien-contre-lintimidation/?pgnc=1">
-            Acadie Nouvelle, 17 novembre 2014, Plus de soutien contre l’intimidation
+            Plus de soutien contre l’intimidation
         </a>
-    </li>
-</ul>
+    </h1>
+    <cite>Acadie Nouvelle</cite>
+    <time>17 novembre 2014</time>
+</article>
 MEDIA_EN;
 
 $body_fr = <<<MEDIA_FR
